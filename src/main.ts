51010633ea92a7175c441f905fb086d88af730b4
DevelopJKong <ybaram40@gmail.com>
2022-06-07 23:18:59 +0900

    #2.6 Validating ConfigService 완료

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const PORT = 3000;
+  const start = () => console.log(`😎 http://localhost:${PORT}`);
+  await app.listen(PORT, start);
 }
 bootstrap();
