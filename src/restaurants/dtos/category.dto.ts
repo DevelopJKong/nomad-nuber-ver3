0a82f46c8aecd4eddc7ee789f804db25d7ab3b10
DevelopJKong <ybaram40@gmail.com>
2022-09-07 22:00:56 +0900

    #10.15 Pagination 완료
    1. 페이지네이션 구현 완료
    2. find() api 에서 take와 skip 에 대해서 정확한 이해 필요

--- src/restaurants/dtos/category.dto.ts
+++ src/restaurants/dtos/category.dto.ts
@@ -1,15 +1,16 @@
 import { CoreOutput } from './../../common/dtos/output.dto';
-import { ArgsType, Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Category } from '../entities/category.entity';
+import { PaginationInput, PaginationOutput } from 'src/common/dtos/pagination.dto';
 
-@ArgsType()
-export class CategoryInput {
+@InputType()
+export class CategoryInput extends PaginationInput {
   @Field((type) => String)
   slug: string;
 }
 
 @ObjectType()
-export class CategoryOutput extends CoreOutput {
+export class CategoryOutput extends PaginationOutput {
   @Field((type) => Category, { nullable: true })
   category?: Category;
 }
