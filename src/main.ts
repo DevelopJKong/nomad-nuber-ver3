b69c9b8fee7aa0d924e456c515e2c796cf5be9cd
DevelopJKong <ybaram40@gmail.com>
2022-09-30 07:14:08 +0900

    프론트를 위해 port 3000 => 5000 으로 변경

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  const PORT = 3000;
+  const PORT = 5000;
   const start = () => console.log(`😎 http://localhost:${PORT}`);
   await app.listen(PORT, start);
 }
