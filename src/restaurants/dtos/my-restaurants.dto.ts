4e83b136a1bcb6b9c8f75007b632fee05065d860
cafe small house <ybaram40@gmail.com>
2022-11-26 17:42:38 +0900

    myRestaurant api 수정

--- src/restaurants/dtos/my-restaurants.dto.ts
+++ src/restaurants/dtos/my-restaurants.dto.ts
@@ -1,6 +1,6 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Restaurant } from '../entities/restaurant.entity';
-import { CoreOutput } from './../../common/dtos/output.dto';
+import { CoreOutput } from '../../common/dtos/output.dto';
 
 @ObjectType()
 export class MyRestaurantsOutput extends CoreOutput {
