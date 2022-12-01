1dfafbac71105334eff6f86fdbcf16dfa35d0c7d
cafe small house <ybaram40@gmail.com>
2022-12-01 22:46:32 +0900

    #23.2 Paddle Integration part One 완료

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -4,9 +4,11 @@ import { Module } from '@nestjs/common';
 import { PaymentService } from './payments.service';
 import { Payment } from './entities/payment.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
+import { PaymentsController } from './payments.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Payment, Restaurant])],
   providers: [PaymentResolver, PaymentService],
+  controllers: [PaymentsController],
 })
 export class PaymentsModule {}
