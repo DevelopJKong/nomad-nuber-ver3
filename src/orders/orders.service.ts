a30262351ca21b30cc2b7fda2d7a16528be7f049
cafe small house <ybaram40@gmail.com>
2022-12-05 12:42:16 +0900

    find 문제 해결

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -67,7 +67,7 @@ export class OrderService {
             if (dishOption.extra) {
               dishFinalPrice = dishFinalPrice + dishOption.extra;
             } else {
-              const dishOptionChoice = dishOption.choices.find(
+              const dishOptionChoice = dishOption.choices?.find(
                 (optionChoice) => optionChoice.name === itemOption.choice,
               );
 
